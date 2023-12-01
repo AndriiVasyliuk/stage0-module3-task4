7a3c3a77b1239bde3a24a5c0da63b118b9fd7fee
AndriiVasyliuk <AndriiVasyliukPro@gmail.com>
2023-12-01 19:34:07 +0200

    [modify] change all the files

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.module3.task4;
 
+// Print chars :'c', 'h', 'a', 'r' using int representation. Implement program in the following code snippet:
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
